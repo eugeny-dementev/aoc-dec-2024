bdb4ca497c405d8b64b4459313275dec7bb4f37d
Eugeny Dementyev <eugeny.dementev@ya.ru>
2024-12-02 21:07:35 +0800

    first attempt on day 2 part 2

--- day2.go
+++ day2.go
@@ -43,17 +43,30 @@ func readReportsInput() [][]int {
 func day2CountSafeReports() {
 	reports := readReportsInput()
 
-  safeCounter := 0
+	safeCounter := 0
+	tolerableCounter := 0
 
 	for _, report := range reports {
 		safe := day2IsReportSafe(report)
+		tolerable := day2IsReportTolerable(report, false)
 
-    if (safe) {
-      safeCounter++
-    }
+		if safe {
+			safeCounter++
+		}
+
+		if tolerable {
+			tolerableCounter++
+		}
+
+		if !safe && tolerable {
+			fmt.Println("Extra report:", report)
+			day2IsReportTolerable(report, true)
+
+		}
 	}
 
-  fmt.Println("Total safe reports:", safeCounter, "out of", len(reports))
+	fmt.Println("Total safe reports:", safeCounter, "out of", len(reports))
+	fmt.Println("Total tolerable reports:", tolerableCounter, "out of", len(reports))
 }
 
 func IntAbs(x int) int {
@@ -90,7 +103,79 @@ func day2IsReportSafe(report []int) bool {
 
 	return true
 }
+
+func isIncreasing(report []int) (bool, bool) {
+  // return true if most of the values are increasing
+	prev := report[0]
+	rest := report[1:]
+
+  inc := 0
+
+  for _, cur := range rest {
+    if prev < cur {
+      inc++
+    } else {
+      inc--
+    }
+  }
+
+  if inc == 0 {
+    return false, true
+  }
+
+  sign := inc / IntAbs(inc)
+
+  return sign > 0, len(report) - IntAbs(inc) > 1
+}
+
+func day2IsReportTolerable(report []int, print bool) bool {
+	prev := report[0]
+	rest := report[1:]
+  max := prev
+
+	increasing, badOrder := isIncreasing(report)
+
+  if badOrder {
+    return false
+  }
+
+	orderFailsCounter := 0
+	diffFailsCounter := 0
+  maxFailsCounter := 0
+
+	for _, cur := range rest {
+		if increasing && prev > cur {
+			orderFailsCounter += 1
+		}
+
+		if !increasing && prev < cur {
+			orderFailsCounter += 1
+		}
+
+    if increasing && max > cur {
+      maxFailsCounter += 1
+    } else {
+      max = cur
+    }
+
+    if !increasing && max < cur {
+      maxFailsCounter += 1
+    } else  {
+      max = cur
+    }
+
+
+		diff := IntAbs(prev - cur)
+		if diff < 1 || diff > 3 {
+			diffFailsCounter += 1
+		}
+
+		prev = cur
 	}
 
-  return true
+	if print {
+		fmt.Println("Failing counters, (order, diff, max)", orderFailsCounter, diffFailsCounter, maxFailsCounter)
+	}
+
+	return orderFailsCounter+diffFailsCounter+maxFailsCounter < 2
 }

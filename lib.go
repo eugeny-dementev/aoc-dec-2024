226803583dbdd7b4babcee411b5b06d18ae848b1
Eugeny Dementyev <eugeny.dementev@ya.ru>
2024-12-04 20:38:47 +0800

    add lib with common methods

---
+++ lib.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	assert "eugeny-dementev/aoc-dec-2024/pkg"
+	"fmt"
+	"os"
+)
+
+func readInput(fileName string) []byte {
+	content, err := os.ReadFile(fileName)
+	assert.NoError(err, fmt.Sprintf("should read %s file without issues", fileName))
+
+	return content
+}

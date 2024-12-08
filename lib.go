17350386ff7a71f3b9f47d09453fadc4339e7cc6
Eugeny Dementyev <eugeny.dementev@ya.ru>
2024-12-08 20:20:38 +0800

    add lib func to read lines

--- lib.go
+++ lib.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"bufio"
 	"bytes"
 	assert "eugeny-dementev/aoc-dec-2024/pkg"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func readInput(fileName string) []byte {
@@ -12,4 +14,18 @@ func readInput(fileName string) []byte {
 	assert.NoError(err, fmt.Sprintf("should read %s file without issues", fileName))
 
 	return bytes.TrimSpace(content)
+}
+
+func readLines(fileName string) []string {
+	input := readInput(fileName)
+
+	var lines []string
+
+	sc := bufio.NewScanner(strings.NewReader(string(input)))
+
+	for sc.Scan() {
+		lines = append(lines, sc.Text())
+	}
+
+	return lines
 }

a31f1158f738bdbcd6662ebf1b765f4b0e8f6f29
Eugeny Dementyev <eugeny.dementev@ya.ru>
2024-12-10 18:49:18 +0800

    wip: day9 part2

--- day9.go
+++ day9.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 var day9example = `2333133121414131402`
 
@@ -67,4 +70,127 @@ func day9TwoPointer() {
 	}
 
 	fmt.Println("Hashsum", hashsum)
+}
+
+func printLine(data []Day9Section) string {
+	var str string
+
+	for _, section := range data {
+		for range section.size {
+			if section.sectionType == DOT {
+				str += "."
+			} else {
+				str += strconv.FormatInt(int64(section.id), 10)
+			}
+		}
+	}
+
+	return str
+}
+
+var DOT, DATA = -1, 1
+
+type Day9Section struct {
+	sectionType int // -1 if "." and 1 if data
+	size        int
+	id          int // -1 if DOT
+}
+
+func (s Day9Section) String() string {
+	t := "."
+	if s.sectionType == DATA {
+		t = strconv.FormatInt(int64(s.id), 10)
+	}
+
+	return fmt.Sprintf("(%v|%v)", t, s.size)
+}
+
+func day9Optimised() {
+	// input := string(readFileInput("day9.txt"))
+	input := day9example
+
+	var data []Day9Section
+
+	id := 0
+
+	for i, c := range input {
+		size := int(c) - '0'
+
+		if i%2 == 0 {
+			data = append(data, Day9Section{DATA, size, id})
+			id++
+		} else if size > 0 {
+			data = append(data, Day9Section{DOT, size, DOT})
+		}
+	}
+
+	fmt.Println("data:", printLine(data))
+
+	left := 0
+
+	right := len(data) - 1
+
+	protector := len(data)
+
+	for right >= 0 && protector >= 0 {
+		for data[right].sectionType == DOT {
+			right--
+		}
+
+		fmt.Println("right is now", data[right])
+
+    leftLoop := true
+		for leftLoop || (data[left].sectionType == DATA || data[right].size > data[left].size) {
+			fmt.Println("left++", left)
+			left++
+
+      if left > len(data)-1 {
+        right--
+        left = 0
+        leftLoop = false
+      }
+		}
+
+    if leftLoop {
+      protector++
+      continue
+    }
+
+		fmt.Println("left is now", data[left])
+
+		if left >= right {
+			break
+		}
+
+		diff := data[left].size - data[right].size
+
+		fmt.Println("data before swap", printLine(data))
+		data[left] = data[right]
+		data[right] = Day9Section{DOT, data[left].size, DOT}
+		if diff > 0 {
+			pos := left + 1
+			data = append(data[:pos], append([]Day9Section{{DOT, diff, DOT}}, data[pos:]...)...)
+		}
+		fmt.Println("data after swap", printLine(data))
+
+		left = 0
+
+		protector--
+	}
+
+	fmt.Println("data:", printLine(data))
+
+	// left = 0
+
+	// var i, hashsum int64
+
+	// for data[i] != dot {
+	// 	c := data[i]
+
+	// 	hashsum += i * int64(c)
+
+	// 	i++
+	// }
+
+	// fmt.Println("Hashsum", hashsum)
 }
